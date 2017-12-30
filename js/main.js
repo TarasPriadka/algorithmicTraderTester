db77fa2992cf6e7df1ff3b6d7065649dfa912873
TarasPriadka <taras.priadka@gmail.com>
2017-12-29 21:50:08 -0800

    Kinda working highlight
    Did highlight, it appends wrong, put all of the tester code into the
    tester.js

--- js/main.js
+++ js/main.js
@@ -1,103 +1,53 @@
-var _wallet = {
-    _usd: 1000,
-    _stock: 0
-}
-var _today = {
-    _time: 0,
-    _price: 0
-}
-
-function getNet() {
-    return _wallet._usd + (_wallet._stock * _today._price)
-}
-
-function buy(amount) {
-    if (isNaN(amount)) return
-    if (amount > _wallet._usd / _today._price) amount = _wallet._usd / _today._price
-    _wallet._stock += amount
-    _wallet._usd -= amount * _today._price
-}
-
-function sell(amount) {
-    if (isNaN(amount)) return
-    if (amount > _wallet._stock) amount = _wallet._stock
-    _wallet._stock -= amount
-    _wallet._usd += amount * _today._price
-}
-
-function getLast(times) {
-    // return prices for the last times
-}
-// vars to be accessed by the user:
-var time = 0
-var price = 0
-var usd = 0
-var stock = 0
-var memory = {}
-// wallet history
-var _walletHistory = []
-
-function _saveWallet() {
-    _walletHistory.push(JSON.parse(JSON.stringify(_wallet)))
-}
-
-function _updateToday() {
-    _today._time += 1
-    _today._price += 100
-    time = _today._time
-    price = _today._price
-    usd = _wallet._usd
-    stock = _wallet._stock
-}
-
-function _runUserCode() {
-    eval(document.getElementById('input').value)
-}
-
-function _reset() {
-    document.getElementById('output').value = ""
-    _today._time = 0
-    _today._price = 0
-    _wallet._usd = 1000
-    _wallet._stock = 0
-    _walletHistory = []
-}
-
-function _execute() {
-    _reset()
-    for (var _i = 0; _i < 10; _i++) {
-        _saveWallet()
-        _updateToday()
-        _runUserCode()
-        var _temp = "TIME: " + _today._time
-        _temp += "\t PRICE: " + _today._price
-        _temp += "\t USD: " + _wallet._usd
-        _temp += "\t STOCK:" + _wallet._stock
-        _temp += "\tNET: " + getNet()
-        _temp += "\n"
-        document.getElementById('output').value += _temp
-    }
-    console.log(_walletHistory)
-}
-
 function _getStringFromDiv(divName) {
     var domString = "",
         temp = "";
-    $(String.format('#{0}',divName)).each(function() {
+    $(String.format('#{0}', divName)).each(function() {
         temp = $(this).html();
         domString += ((temp == "<br>") ? "" : temp);
     });
     return domString;
 }
-
 if (!String.format) {
-  String.format = function(format) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return format.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number] 
-        : match
-      ;
+    String.format = function(format) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        return format.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
+}
+
+$(document).ready(function() {
+    $('#input').on("input", function() {
+        _spanDiv();
     });
-  };
+});
+
+function _spanDiv() {
+    var original = $('#input');
+    var obj = $('#input');
+    var text = obj.text();
+    text = text.split(" ");
+    var parsed = "";
+    obj.empty();
+    for (var i = 0; i < text.length; i++) {
+        var tag = document.createElement('span');
+        tag.setAttribute('class', 'vtag');
+        tag.innerHTML = text[i] + " ";
+        var key = isKey(text[i]);
+        if(key){
+        	tag.setAttribute('style', 'color:red;');
+        }
+        
+        
+        
+        obj.append(tag);
+    }
+}
+
+function isKey(word) {
+    var jskeywords = ['alert','abstract', 'arguments', 'await', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class', 'const', 'continue', 'debugger', 'default', 'delete', 'do', 'double', 'else', 'enum', 'eval', 'export', 'extends', 'false', 'final', 'finally', 'float', 'for', 'function', 'goto', 'if', 'implements', 'import', 'in', 'instanceof', 'int', 'interface', 'let', 'long', 'native', 'new', 'null', 'package', 'private', 'protected', 'public', 'return', 'short', 'static', 'super', 'switch', 'synchronized', 'this', 'throw', 'throws', 'transient', 'true', 'try', 'typeof', 'var', 'void', 'volatile', 'while', 'with', 'yield'];
+    if (jskeywords.includes(word))
+    	return true;
+    else
+    	return false;
 }
---
+++ js/tester.js
@@ -0,0 +1,81 @@
+var _wallet = {
+    _usd: 1000,
+    _stock: 0
+}
+var _today = {
+    _time: 0,
+    _price: 0
+}
+
+function getNet() {
+    return _wallet._usd + (_wallet._stock * _today._price)
+}
+
+function buy(amount) {
+    if (isNaN(amount)) return
+    if (amount > _wallet._usd / _today._price) amount = _wallet._usd / _today._price
+    _wallet._stock += amount
+    _wallet._usd -= amount * _today._price
+}
+
+function sell(amount) {
+    if (isNaN(amount)) return
+    if (amount > _wallet._stock) amount = _wallet._stock
+    _wallet._stock -= amount
+    _wallet._usd += amount * _today._price
+}
+
+function getLast(times) {
+    // return prices for the last times
+}
+// vars to be accessed by the user:
+var time = 0
+var price = 0
+var usd = 0
+var stock = 0
+var memory = {}
+// wallet history
+var _walletHistory = []
+
+function _saveWallet() {
+    _walletHistory.push(JSON.parse(JSON.stringify(_wallet)))
+}
+
+function _updateToday() {
+    _today._time += 1
+    _today._price += 100
+    time = _today._time
+    price = _today._price
+    usd = _wallet._usd
+    stock = _wallet._stock
+}
+
+function _runUserCode() {
+    eval(_getStringFromDiv('input'))
+}
+
+function _reset() {
+    document.getElementById('output').value = ""
+    _today._time = 0
+    _today._price = 0
+    _wallet._usd = 1000
+    _wallet._stock = 0
+    _walletHistory = []
+}
+
+function _execute() {
+    _reset()
+    for (var _i = 0; _i < 10; _i++) {
+        _saveWallet()
+        _updateToday()
+        _runUserCode()
+        var _temp = "TIME: " + _today._time
+        _temp += "\t PRICE: " + _today._price
+        _temp += "\t USD: " + _wallet._usd
+        _temp += "\t STOCK:" + _wallet._stock
+        _temp += "\tNET: " + getNet()
+        _temp += "\n"
+        document.getElementById('output').value += _temp
+    }
+    console.log(_walletHistory)
+}
